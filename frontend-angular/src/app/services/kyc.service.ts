3862eeb7ab87c6d5de7154c9aae2bd53f6c8b1ea
dheerajdrse7en <dheerajdrse7en@gmail.com>
2025-06-26 10:55:56 +0530

    final

--- frontend-angular/src/app/services/kyc.service.ts
+++ frontend-angular/src/app/services/kyc.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class KycService {
-  private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = 'https://kyc-verifier-rzca.onrender.com/api';
 
   constructor(private http: HttpClient) {}
 
